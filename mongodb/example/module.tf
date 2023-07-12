c98478806eb933b2c0ea8719ad883cdff98f06ab
EC2 Default User <ec2-user@ip-172-31-19-139.ec2.internal>
2023-07-12 07:27:48 +0000

    added example for calling the module

--- mongodb/example/module.tf
+++ mongodb/example/module.tf
@@ -4,10 +4,10 @@ module "mongodb" {
 
   region            = "us-east-1"
   instance_type     = "t2.micro"
-  key_name          = "mongodb"
-  vpc_id = ""
-  subnet_id         = "subnet-0e1a6877842dfe1bf"
-  private_subnet_id = "subnet-02d9b27100c33fbdb"
+  key_name          = "satyam-terraform-nvirginia"
+  vpc_id            = "vpc-0803d89825ac7428d"
+  subnet_id         = "subnet-027eb9ffb346d7806"
+  private_subnet_id = "subnet-027eb9ffb346d7806"
   secondary_count   = 2
   replica_set       = "my-replica-set"
   mongo_username    = "admin"
