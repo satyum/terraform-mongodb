d5605421c958b35f11a4d23fc1dc512a67d26c16
satyum <kingsatyam7@gmail.com>
2023-11-27 12:33:41 +0530

    restructing the module

--- example/module.tf
+++ example/module.tf
@@ -5,11 +5,10 @@ module "mongodb" {
   region            = "us-east-1"
   instance_type     = "t3.medium"
   key_name          = "satyam-terraform-nvirginia"
-  kms_key_id        = "12be95c4-144f-47a9-beaa-67693dc586d7"
   vpc_id            = "vpc-0803d89825ac7428d"
   vpc_cidr_block = "0.0.0.0/0"
-  subnet_id         = "subnet-027eb9ffb346d7806"
-  private_subnet_id = "subnet-027eb9ffb346d7806"
+  subnet_id         = "subnet-027xxxx"
+  private_subnet_id = "subnet-027xxxxx"
   secondary_count   = 2
   replica_set       = "my-replica-set"
   mongo_username    = "admin"
